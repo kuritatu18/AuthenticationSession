e98374719bb157287bf5d85169af2f6cb92a1a09
Tatsunori Kuri <kuritatu18@gmail.com>
2019-01-03 01:22:43 +0900

    add getAuthorizationToken

--- AuthenticationSession/ViewController.swift
+++ AuthenticationSession/ViewController.swift
@@ -8,18 +8,33 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
+    
+    var authenticationSession: AuthenticationSession?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // generate customzURL link
+        let url = URL(string: "http://ux.nu/uS21P")!
+        
+        authenticationSession = AuthenticationSession.init(url: url, callbackURLScheme: nil, completionHandler: { [weak self] (callBack: URL?, error: Error?) in
+            self?.getAuthorizationToken(callBack: callBack, error: error)
+        })
+        _ = authenticationSession?.start()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func getAuthorizationToken(callBack: URL?, error: Error?) {
+        guard error == nil,
+            let successURL = callBack else {
+                return
+        }
+        let authToken = NSURLComponents(string: (successURL.absoluteString))?.queryItems?.filter({$0.name == "token"}).first
+        guard let _authToken = authToken else {
+            return
+        }
+        print(String(describing: _authToken.value))
     }
-
-
 }
 
